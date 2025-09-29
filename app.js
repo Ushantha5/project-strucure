542bc992e1e187b67a1fe6dba4d100dfb2441f7e
ushantha <ushanthamr@gmail.com>
2025-09-29 15:33:13 +0530

    useing API

--- app.js
+++ app.js
@@ -1,73 +1,18 @@
 // Basic server setup
 import express, { Router } from "express";
 
+import userRoutes from "./router/userRouter.js";
+
 const app = express();
 const PORT = 3000;
-const router = express.Router();
 
 // HTTP method handling
 app.get("/", (req, res) => {
   res.send("Hello Express!");
 });
 
-router.get("/users", (req, res) => {
-  res.send("This is GET User...");
-});
-
-router.get("/users/:id", (req, res) => {
-  res.send("This is GET one User...");
-});
-
-router.post("/users", (req, res) => {
-  res.send("This is Create User...");
-});
-
-router.put("/users/:id", (req, res) => {
-  res.send("This is put one User...");
-});
-
-router.delete("/users/:id", (req, res) => {
-  res.send("This is DELETE User...");
-});
-
-app.use(router);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
-// // Basic server setup
-// import express from 'express'
-
-// const app = express()
-// const router = express.Router();
-
-// const PORT = 3000;
-
-// // HTTP method handling
-// app.get('/', (req, res) => {
-//     res.send('Hello Express!');
-// });
-
-// // users routes
-// router.get('/users', (req, res) => {
-//     res.send('This is GET Users...')
-// })
-// router.get('/users/:id', (req, res) => {
-//     res.send('This is GET one User...')
-// })
-// router.post('/users', (req, res) => {
-//     res.send('This is Create Users...')
-// })
-// router.put('/users/:id', (req, res) => {
-//     res.send('This is Update User...')
-// })
-// router.delete('/users/:id', (req, res) => {
-//     res.send('This is Delete User...')
-// })
-
-// app.use(router)
-
-// app.listen(PORT, () => {
-//     console.log(`Server running at http://localhost:${PORT}`);
-// });
---
+++ router/userRouter.js
@@ -0,0 +1,25 @@
+import express, { Router } from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("This is GET User...");
+});
+
+router.get("/:id", (req, res) => {
+  res.send("This is GET one User...");
+});
+
+router.post("/", (req, res) => {
+  res.send("This is Create User...");
+});
+
+router.put("/:id", (req, res) => {
+  res.send("This is put one User...");
+});
+
+router.delete("/:id", (req, res) => {
+  res.send("This is DELETE User...");
+});
+
+export default router;
